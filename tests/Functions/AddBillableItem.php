fcf1a767d7c88770650ec8f979bf154e894351fe
Peter Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2020-03-06 14:42:51 +0000

    Add support for AddBillableItem (#8)

---
+++ tests/Functions/AddBillableItem.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WHMCSAPI\Tests\Functions;
+
+use WHMCSAPI\Exception\NotServiceable;
+use WHMCSAPI\Tests\BaseTest;
+
+class AddBillableItem extends BaseTest
+{
+    public function testCanUseAddBillableItemCommand()
+    {
+        $GLOBALS['whmcsApi']->command('AddBillableItem');
+        $this->assertEquals('AddBillableItem', $GLOBALS['whmcsApi']->action);
+    }
+
+    public function testNoClientIDCauseException()
+    {
+        $this->assertNull($GLOBALS['whmcsApi']->clientid);
+        $this->expectException(NotServiceable::class);
+        $GLOBALS['whmcsApi']->execute();
+    }
+
+    public function testAttributesCanBeSet()
+    {
+        $GLOBALS['whmcsApi']->clientid = '1';
+        $GLOBALS['whmcsApi']->description = 'PHPUnit Testing';
+        $GLOBALS['whmcsApi']->amount = 7.50;
+        $GLOBALS['whmcsApi']->invoiceaction = 'noinvoice';
+        $GLOBALS['whmcsApi']->recur = 6;
+        $GLOBALS['whmcsApi']->recurcycle = 'Days';
+        $GLOBALS['whmcsApi']->recurfor = 2;
+        $GLOBALS['whmcsApi']->duedate = date('Y-m-d H:i:s');
+        $GLOBALS['whmcsApi']->hours = 2;
+        $this->assertEquals('PHPUnit Testing', $GLOBALS['whmcsApi']->description);
+    }
+
+    public function testCanMakeAPICall()
+    {
+        $result = $GLOBALS['whmcsApi']->execute();
+        $this->assertStringContainsString('{"result":', $result);
+    }
+}
