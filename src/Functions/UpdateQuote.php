f8d58411211b364f8432e563a315e8fdd53d8773
Marcus Allen <mallen@overwatchdata.com>
2020-03-07 17:27:05 -0600

    Updated for PSR-12

--- src/Functions/UpdateQuote.php
+++ src/Functions/UpdateQuote.php
@@ -4,41 +4,41 @@ namespace WHMCSAPI\Functions;
 
 class UpdateQuote
 {
-	public static $action = 'UpdateQuote';
+    public static $action = 'UpdateQuote';
 
-	public const ATTRIBUTES = [
-		'quoteid',
-		'subject',
-		'stage',
-		'validuntil',
-		'datecreated',
-		'lineitems',
-		'userid',
-		'firstname',
-		'lastname',
-		'companyname',
-		'email',
-		'address1',
-		'address2',
-		'city',
-		'state',
-		'country',
-		'phonenumber',
-		'tax_id',
-		'currency',
-		'proposal',
-		'customernotes',
-		'adminnotes',
-	];
+    public const ATTRIBUTES = [
+        'quoteid',
+        'subject',
+        'stage',
+        'validuntil',
+        'datecreated',
+        'lineitems',
+        'userid',
+        'firstname',
+        'lastname',
+        'companyname',
+        'email',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'country',
+        'phonenumber',
+        'tax_id',
+        'currency',
+        'proposal',
+        'customernotes',
+        'adminnotes',
+    ];
 
-	public const REQUIRED_ATTRIBUTES = [
-		'quoteid',
-	];
+    public const REQUIRED_ATTRIBUTES = [
+        'quoteid',
+    ];
 
-	public const ADDITIONAL_REQUIREMENTS = [
-		'quoteid' => 'numeric',
-		'lineitems' => 'array',
-		'userid' => 'numeric',
-		'currency' => 'numeric',
-	];
+    public const ADDITIONAL_REQUIREMENTS = [
+        'quoteid' => 'numeric',
+        'lineitems' => 'array',
+        'userid' => 'numeric',
+        'currency' => 'numeric',
+    ];
 }
--- tests/Functions/UpdateQuoteTest.php
+++ tests/Functions/UpdateQuoteTest.php
@@ -7,30 +7,30 @@ use WHMCSAPI\Tests\BaseTest;
 
 class UpdateQuoteTest extends BaseTest
 {
-	public function testCanUseUpdateQuoteCommand()
-	{
-		$GLOBALS['whmcsApi']->command('UpdateQuote');
-		$this->assertEquals('UpdateQuote', $GLOBALS['whmcsApi']->action);
-	}
+    public function testCanUseUpdateQuoteCommand()
+    {
+        $GLOBALS['whmcsApi']->command('UpdateQuote');
+        $this->assertEquals('UpdateQuote', $GLOBALS['whmcsApi']->action);
+    }
 
-	public function testNoQuoteIDCauseException()
-	{
-		$this->assertNull($GLOBALS['whmcsApi']->quoteid);
-		$this->expectException(NotServiceable::class);
-		$GLOBALS['whmcsApi']->execute();
-	}
+    public function testNoQuoteIDCauseException()
+    {
+        $this->assertNull($GLOBALS['whmcsApi']->quoteid);
+        $this->expectException(NotServiceable::class);
+        $GLOBALS['whmcsApi']->execute();
+    }
 
-	public function testAttributesCanBeSet()
-	{
-		$GLOBALS['whmcsApi']->stage = 'Accepted';
-		$GLOBALS['whmcsApi']->subject = 'ABC123';
-		$this->assertEquals('Accepted', $GLOBALS['whmcsApi']->stage);
-		$this->assertEquals('ABC123', $GLOBALS['whmcsApi']->subject);
-	}
+    public function testAttributesCanBeSet()
+    {
+        $GLOBALS['whmcsApi']->stage = 'Accepted';
+        $GLOBALS['whmcsApi']->subject = 'ABC123';
+        $this->assertEquals('Accepted', $GLOBALS['whmcsApi']->stage);
+        $this->assertEquals('ABC123', $GLOBALS['whmcsApi']->subject);
+    }
 
-	public function testCanMakeAPICall()
-	{
-		$result = $GLOBALS['whmcsApi']->execute();
-		$this->assertStringContainsString('{"result":', $result);
-	}
+    public function testCanMakeAPICall()
+    {
+        $result = $GLOBALS['whmcsApi']->execute();
+        $this->assertStringContainsString('{"result":', $result);
+    }
 }
