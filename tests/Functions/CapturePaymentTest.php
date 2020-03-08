a0fdcffc1a0fc48c633fbcc1abe2b66eee51d2a1
Peter Bishop <peter.bishop@sysadmin999.com>
2020-03-08 21:40:56 +0000

    Add CapturePayment

---
+++ tests/Functions/CapturePaymentTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WHMCSAPI\Tests\Functions;
+
+use WHMCSAPI\Exception\NotServiceable;
+use WHMCSAPI\Tests\BaseTest;
+
+class CapturePaymentTest extends BaseTest
+{
+
+    public function testCanUseAcceptOrderCommand()
+    {
+        $GLOBALS['whmcsApi']->command('CapturePayment');
+        $this->assertEquals('CapturePayment', $GLOBALS['whmcsApi']->action);
+    }
+
+    public function testNoOrderIDCauseException()
+    {
+        $this->assertNull($GLOBALS['whmcsApi']->invoiceid);
+        $this->expectException(NotServiceable::class);
+        $GLOBALS['whmcsApi']->execute();
+    }
+
+    public function testAttributesCanBeSet()
+    {
+        $GLOBALS['whmcsApi']->invoiceid = 1;
+        $GLOBALS['whmcsApi']->cvv = '024';
+        $this->assertEquals(1, $GLOBALS['whmcsApi']->invoiceid);
+    }
+
+    public function cvvPassesNumericalValidationStartingZero()
+    {
+        $this->assertTrue($GLOBALS['whmcsApi']->inputValidate('numeric', $GLOBALS['whmcsApi']->cvv));
+        $this->assertEquals('024', $GLOBALS['whmcsApi']->cvv);
+    }
+
+    public function testCanMakeAPICall()
+    {
+        $result = $GLOBALS['whmcsApi']->execute();
+        $this->assertStringContainsString('{"result":', $result);
+    }
+}
