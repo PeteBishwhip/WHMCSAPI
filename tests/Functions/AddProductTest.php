d071d27b0c706afdd900489d667a1e1ea462f379
Peter Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2020-03-08 17:33:09 +0000

    Add AddProduct (#167)
    * Add AddProduct
    
    * Rectify class in AddProductTest

---
+++ tests/Functions/AddProductTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WHMCSAPI\Tests\Functions;
+
+use WHMCSAPI\Exception\NotServiceable;
+use WHMCSAPI\Tests\BaseTest;
+
+class AddProductTest extends BaseTest
+{
+    public function testCanUseUpdateQuoteCommand()
+    {
+        $GLOBALS['whmcsApi']->quoteid = 1;
+        $GLOBALS['whmcsApi']->command('AddProduct');
+        $this->assertEquals('AddProduct', $GLOBALS['whmcsApi']->action);
+    }
+
+    public function testNoProductNameCauseException()
+    {
+        $this->assertNull($GLOBALS['whmcsApi']->name);
+        $this->expectException(NotServiceable::class);
+        $GLOBALS['whmcsApi']->execute();
+    }
+
+    public function testDefaultValuesAreSet()
+    {
+        $class = '\\WHMCSAPI\\Functions\\AddProduct';
+        if (defined($class . '::DEFAULTS')) {
+            foreach ($class::DEFAULTS as $attribute => $default) {
+                $this->assertEquals($default, $GLOBALS['whmcsApi']->$attribute);
+            }
+        } else {
+            $this->addToAssertionCount(1);
+        }
+    }
+
+    public function testAttributesCanBeSet()
+    {
+        $GLOBALS['whmcsApi']->gid = 1;
+        $GLOBALS['whmcsApi']->name = 'Test Product';
+        $this->assertEquals(1, $GLOBALS['whmcsApi']->gid);
+        $this->assertEquals('Test Product', $GLOBALS['whmcsApi']->name);
+    }
+
+    public function testCanMakeAPICall()
+    {
+        $result = $GLOBALS['whmcsApi']->execute();
+        $this->assertStringContainsString('{"result":', $result);
+    }
+}
