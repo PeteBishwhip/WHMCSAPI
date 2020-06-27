a79a8fe2e746e9ecc5c6d439c82e0dac28f23cbc
Peter Bishop <peter.bishop@sysadmin999.com>
2020-06-27 19:55:32 +0100

    Implement DomainGetLockingStatus Issue #48
    * Add required files for DomainGetLockingStatus
    * Implement test

---
+++ tests/Functions/DomainGetLockingStatusTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WHMCSAPI\Tests\Functions;
+
+use WHMCSAPI\Exception\NotServiceable;
+use WHMCSAPI\Tests\BaseTest;
+
+class DomainGetLockingStatusTest extends BaseTest
+{
+
+    public function testCanUseDomainGetLockingStatusCommand()
+    {
+        $GLOBALS['whmcsApi']->command('DomainGetLockingStatus');
+        $this->assertEquals('DomainGetLockingStatus', $GLOBALS['whmcsApi']->action);
+    }
+
+    public function testNoIDCauseException()
+    {
+        $this->assertNull($GLOBALS['whmcsApi']->domainid);
+        $this->expectException(NotServiceable::class);
+        $GLOBALS['whmcsApi']->execute();
+    }
+
+    public function testAttributesCanBeSet()
+    {
+        $GLOBALS['whmcsApi']->domainid = 1;
+        $this->assertEquals(1, $GLOBALS['whmcsApi']->domainid);
+    }
+
+    public function testCanMakeAPICall()
+    {
+        $result = $GLOBALS['whmcsApi']->execute();
+        $this->assertJson($result);
+        $result = (json_decode($result, true))['postData'];
+        $this->assertArrayHasKey('domainid', $result);
+        unset($result['username'], $result['password'], $result['responsetype']);
+        foreach ($result as $attribute => $value) {
+            $this->assertEquals($GLOBALS['whmcsApi']->{$attribute}, $value);
+        }
+    }
+}
